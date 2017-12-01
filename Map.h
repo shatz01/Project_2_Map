ceee9530ed0f0371ae445d8cfaa972829f202df0
Daniel Shats <danielshats1@gmail.com>
2017-12-01 15:01:35 -0500

    Made base abstract map. Need to start on first implementation

---
+++ Map.h
@@ -0,0 +1,26 @@
+#ifndef _MAP_H_
+#define _MAP_H_
+namespace cop3530{
+template <typename K, typename V, (*comparison_fn)(E key1)(E key2), (*equality_fn)(E key1)(E key2)>
+class Map
+{
+public:
+  virtual void insert (K key, V val) = 0;
+  virtual V remove(K key) = 0;
+  virtual V& lookup(K key) = 0;
+  virtual bool contains(K key) = 0;
+  virtual bool is_empty() = 0;
+  virtual bool is_full() = 0;
+  virtual size_t size() = 0; // 	returns the number of key-value pairs in the map a size_t.
+  virtual void clear() = 0; // removes all key-value pairs from the map
+  virtual size_t height() = 0;
+  virtual int balance() = 0;
+  // virtual LOAD???
+  //
+  // virtual void print(std::ostream & out) = 0;
+  // virtual bool contains(E elt, bool (*equals_fn)(const E &a, const E &b)) = 0;
+};
+
+
+}
+#endif
